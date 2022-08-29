ac882b933ca85e128331eb2485485a42b6236210
Olayinka Bidemi Ayeni <65237847+YinkTech@users.noreply.github.com>
2022-08-29 08:55:48 +0100

    remove comments

--- src/fetch.js
+++ src/fetch.js
@@ -5,7 +5,6 @@ let filteredData = [];
 export const getData = () => {
     fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`).then(function(response){
         response.json().then(function(data) {
-          //  console.log(data);
             rawData = data;
             filteredData = rawData;
             displayCoins(filteredData);
